7517233379291ec996a5fd0da68fc156b04261a5
ve12sus <jeff@ve12sus.com>
2015-02-11 19:29:15 -0500

    script updated with external json

--- js/ytcscript.js
+++ js/ytcscript.js
@@ -1,3 +1,10 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "../comments.json";
+
+xmlhttp.open("GET", url, false);
+xmlhttp.send();
+
+var myArr = JSON.parse(xmlhttp.responseText);
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
@@ -9,7 +16,7 @@ function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     height: '390',
     width: '640',
-    videoId: obj.id, 
+    videoId: myArr.id, 
     events: {
 	  'onReady': onPlayerReady,
 	  'onReady': setInterval(commentLoad, 1000)
@@ -21,22 +28,14 @@ function onPlayerReady(event) {
   event.target.playVideo();
 }
 
-var obj = {
-  "id": 'HGfC4CFBAns',
-  "comments": [
-    [10, 'new comment'],
-    [24, 'infiltration jump-in']
-  ]
-}
-
 function commentLoad(event) {
   var time = player.getCurrentTime();
-  for (var i = 0; i < obj.comments.length; i++) {
-    if (time >= obj.comments[i][0])
-	  document.getElementById('comment').innerHTML = obj.comments[i][1];
+  for (var i = 0; i < myArr.comments.length; i++) {
+    if (time >= myArr.comments[i][0])
+	  document.getElementById('comment').innerHTML = myArr.comments[i][1];
   } 
   clearInterval(commentLoad);		
 }
 	  
-
+	
  
