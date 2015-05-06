39ac7f0845e89a74f0fd46d5f048219a8338b612
ve12sus <jeff@ve12sus.com>
2015-05-06 14:35:15 -0400

    new feature, parse youtube url/save new json file

--- js/ytcscript.js
+++ js/ytcscript.js
@@ -1,5 +1,19 @@
+var qs = (function(a) {
+    if (a == "") return {};
+    var b = {};
+    for (var i = 0; i < a.length; ++i)
+    {
+        var p=a[i].split('=', 2);
+        if (p.length == 1)
+            b[p[0]] = "";
+        else
+            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
+    }
+    return b;
+})(window.location.search.substr(1).split('&'));
+
 var xmlhttp = new XMLHttpRequest();
-var url = "http://localhost/ytcserver/server.php";
+var url = "http://localhost/ytcserver/server.php" + "?vid=" + qs["vid"];
 
 xmlhttp.open("GET", url, false);
 xmlhttp.send();
--- server.php
+++ server.php
@@ -1,5 +1,5 @@
 <?php
-$file = 'comments.json';
+$file = htmlspecialchars($_GET["vid"]) . '.json';
 $str = file_get_contents($file);
 echo $str;
 ?>
