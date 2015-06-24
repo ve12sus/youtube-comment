ce77e0fffbd6eebab3d30c17e1d3a6ada936f280
Jeff Lo <jeff@ve12sus.com>
2015-06-24 15:16:37 -0400

    first working version shown to Eric

--- js/player.js
+++ js/player.js
@@ -7,16 +7,18 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 // Replace the 'ytplayer' element with an <iframe> and
 // YouTube player after the API code downloads.
 var player;
-setTimeout(function onYouTubeIframeAPIReady() {
-  player = new YT.Player('ytplayer', {
-    height: '390',
-    width: '640',
-    videoId: parsedData.id, 
-	events: {
-	  'onReady' : onPlayerReady,
-	}
-  });
-}, 1000);
+function startPlayer() {
+  window.onYouTubeIframeAPIReady = function() {
+    player = new YT.Player('ytplayer', {
+      height: '390',
+      width: '640',
+      videoId: parsedData.id, 
+	  events: {
+	    'onReady' : onPlayerReady,
+	  }
+    });
+  }
+}
 
 function onPlayerReady(event) {
   event.target.playVideo();
@@ -26,9 +28,11 @@ function onPlayerReady(event) {
 function commentLoad() {
   playerTime = Math.round(player.getCurrentTime());
   console.log(playerTime);
-  for (var i = 0; i < parsedData.comments.length; i ++) {
-    if (playerTime >= parsedData.comments[i][0]) {
-	  document.getElementById('currentComment').innerHTML = parsedData.comments[i][1];
+  sortedComments = parsedData.comments.sort();
+  console.log(sortedComments);
+  for (var i = 0; i < sortedComments.length; i ++) {
+    if (playerTime >= sortedComments[i][0]) {
+	  document.getElementById('currentComment').innerHTML = sortedComments[i][1];
     }
   }
 }
--- js/xhrtest.js
+++ js/xhrtest.js
@@ -4,14 +4,25 @@ function processData() {
   // taking care of data
 //  parsedData = JSON.parse(data);
   for (i = 0; i < parsedData.comments.length; i++) {
-    var listItem = document.createElement("li");
-    var node = document.createTextNode(
-    parsedData.comments[i][0] + " " + parsedData.comments[i][1]);
-    listItem.appendChild(node);
-
-    var element = document.getElementById("objView");
-    element.appendChild(listItem);
-  }  
+	var listItemId = parsedData.comments[i][0].toString();
+	var commentExists = document.getElementById(listItemId);
+	if (typeof(commentExists) != 'undefined' && commentExists !=null){ 
+	  continue; 
+	} else {
+	  var listItem = document.createElement("li");
+      var node = document.createTextNode(
+      secondsToHms(parsedData.comments[i][0]) + " " + parsedData.comments[i][1]);
+      listItem.appendChild(node);
+	  listItem.setAttribute("id", listItemId);
+	  console.log(typeof(listItem.id));
+	  listItem.onclick = function () { 
+	    this.parentElement.removeChild(this); 
+	  }
+      var element = document.getElementById("objView");
+      element.appendChild(listItem);
+	  //document.getElementById(listItemId).onclick = removeComment(listItemId);
+    }
+  }   
 }
 
 function handler() {
@@ -20,6 +31,7 @@ function handler() {
     // success!
 	parsedData = JSON.parse(this.responseText);
     processData();
+	startPlayer();
   } else {
     // something went wrong
     console.log("something went wrong"); 
@@ -32,11 +44,26 @@ client.open("GET", "comments.json");
 client.send();
 
 function newCommentSubmit() {
-  commentTime = Math.round(player.getCurrentTime())e
-  parsedData.comments.push([commentTime, "infiltration loses"]);
+  var commentTime = Math.floor(player.getCurrentTime())
+  var commentText = document.getElementById("commentField").value 
+  parsedData.comments.push([commentTime, commentText]);
   document.getElementById('demo2').innerHTML = parsedData.comments;
+  processData();
 } 
+
+function removeComment(id) {
+  var elementToRemove = document.getElementById(id);
+  elementToRemove.parentNode.removeChild(elementToRemove);
+}
 
 function newDate() {
   document.getElementById('demo1').innerHTML = player.getCurrentTime();
 }  
+
+function secondsToHms(d) {
+  d = Number(d);
+  var h = Math.floor(d / 3600);
+  var m = Math.floor(d % 3600 / 60);
+  var s = Math.floor(d % 3600 % 60);
+  return ((h > 0 ? h + ":" + (m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s); }
+
