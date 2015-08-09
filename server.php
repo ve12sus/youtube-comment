3d75e424a8f35a73e1216b691280516806c7dd46
Jeff Lo <jeff@ve12sus.com>
2015-08-09 03:14:45 -0400

    get working

--- server.php
+++ server.php
@@ -1,50 +1,42 @@
 <?php
-$utility_obj = new RestUtils;
-$new_request = $utility_obj->processRequest();
-$controller = new VidController;
-switch($new_request->getMethod()) {
+$new_request = RestUtils::processRequest();
+$video = new VideoController;
 
+if ($new_request->getResource() == 'videos') {
+	switch($new_request->getMethod()) {
 	case 'get':
-		if (resource == 'videos') {
-			$utility_obj->sendResponse($status = 200, $body = $controller->getVideos(),
-									   $content_type = 'application/json');
-		}
-		if (resource == 'id') {
-			//get $id with router
-			$utility_obj->sendResponse($status = 200, $body = $controller->getVideo($id),
-									   $content_type = 'application/json');
-		}
+		$video->selectVideos();
+		//echo json_encode($video->getObj(), JSON_PRETTY_PRINT);
+		RestUtils::sendResponse(200, $video->getObj());
 		break;
-
+	default:
+		echo 'default message';
+		break;
+	}
 }
+
 class RestUtils {
-	public function processRequest() {
+	public static function processRequest() {
 		$request_method = strtolower($_SERVER['REQUEST_METHOD']);
-		$request_uri 	= $_SERVER['REQUEST_URI'];
-		$request_body	= file_get_contents('php://input');
-		$return_obj		= new clientRequest();
+		$data			= file_get_contents('php://input');
+		$return_obj		= new ClientRequest();
 
 		$return_obj->setMethod($request_method);
-		$return_obj->setURI($request_uri);
-		$return_obj->setRequestBody(json_decode($request_body));
+		$return_obj->setRequestVars();
+		$return_obj->setData($data);
 
 		return $return_obj;
 	}
 
-	public function sendResponse($status = 200, $body = '',
+	public static function sendResponse($status = 200, $object,
 								 $content_type = 'application/json') {
-		$status_header = 'HTTP/1.1 '. $status. ' '. $this->getStatusCodeMsg($status);
+		$status_header = 'HTTP/1.1 '. $status. ' '. RestUtils::getStatusCodeMsg($status);
 		header($status_header);
 		header("'Content-Type: ' . $content_type");
-		if ($body != '') {
-			echo $body;
-			exit;
-		} else {
-			//todo make switch with different status and message body
-		}
+		echo json_encode($object, JSON_PRETTY_PRINT);
 	}
 
-	public function getStatusCodeMsg($status) {
+	public static function getStatusCodeMsg($status) {
 		$codes = array(
 				 100 => 'Continue',
 				 200 => 'OK',
@@ -56,75 +48,94 @@ class RestUtils {
 				 );
 		return (isset($codes[$status])) ? $codes[$status] : '';
 	}
-
-	public function getResource($url) {
-		$uri = parse_url($url);
-		return $uri['path'];
-	}
 }
 
-class clientRequest {
+class ClientRequest {
 	private $method;
-	private $uri;
-	private $request_body;
+	private $resource;
+	private $id;
+	private $data;
 
 	public function __construct() {
 		$this->method 		= 'get';
-		$this->uri		 	= '';
-		$this->request_body = array();
+		$this->resource		= '';
+		$this->id			= array();
+		$this->data 		= array();
 	}
 
 	public function setMethod($method) {
 		$this->method = $method;
 	}
 
-	public function setURI($uri) {
-		$this->uri = $uri;
+	public function setRequestVars() {
+		$request_URI = explode('/',$_SERVER['REQUEST_URI']);
+		$script_name = explode('/', $_SERVER['SCRIPT_NAME']);
+			for ($i=0; $i < sizeof($script_name); $i++) {
+				if ($request_URI[$i] == $script_name[$i]) {
+					unset($request_URI[$i]);
+					}
+			}
+		$rest_URI = array_values($request_URI);
+		$rest_resource = $rest_URI[0];
+		$rest_id = array_slice($request_URI, 1);
+		$this->resource = $rest_resource;
+		$this->id = $rest_id;
 	}
-	public function setRequestBody($request_body) {
-		$this->request_body = $request_body;
+
+	public function setData($data) {
+		$this->data = $data;
 	}
 
 	public function getMethod() {
 		return $this->method;
 	}
 
-	public function getURI() {
-		return $this->uri;
+	public function getResource() {
+		return $this->resource;
 	}
 
-	public function getRequestBody() {
-		return $this->request_body;
+	public function getId() {
+		return $this->id;
+	}
+
+	public function getData() {
+		return $this->data;
 	}
 }
 
 class VideoController {
-	private $video_obj = array();
-	private $comments  = array();
+	private $video_obj;
+	private $comments;
 
-	public function getVideos() {
-		$new_video = new video;
-		$sql = "SELECT * FROM videos";
-		$new_db_connection = new dbConnect();
-		$result = $new_db_connection->query($sql);
-		while ($row = $result->fetch_assoc()) {
-			$video_obj[] = $row;
-		}
-		return $video_obj;
+	public function getObj() {
+		return $this->video_obj;
 	}
 
-	public function getVideo($id) {
-		$sql = "SELECT * from videos WHERE id = $id";
-		$new_db_conn = new dbConnect();
-		$result = $new_db_conn->query($sql);
-		$video_obj = $result->fetch_assoc();
-		$sql = "SELECT * from comments where id = $id";
-		$result = $new_db_conn->query($sql);
+	public function selectVideos() {
+		$sql = "SELECT * from videos";
+		$new_conn = $this->dbConnect();
+		$result = $new_conn->query($sql);
 		while ($row = $result->fetch_assoc()) {
-			$comments[] = $row;
+			$this->video_obj[] = $row;
 		}
-		$this->video_obj['comments'][] =  $comments;
-		return $video_obj;
+	}
+
+	public function selectVideo() {
+	}
+
+	public function createVideo() {
+	}
+
+	public function updateVideo() {
+	}
+
+	public function createComment() {
+	}
+
+	public function updateComment() {
+	}
+
+	public function deleteComment() {
 	}
 
 	public function dbConnect() {
@@ -140,53 +151,4 @@ class VideoController {
 		return $connection;
 	}
 }
-
-class Video {
-	private $video_obj = array();
-	private $comments  = array();
-
-	public function set_video($id, $title, $youtubeId) {
-		$this->video_obj['id'] = $id;
-		$this->video_obj['title'] = $title;
-		$this->video_obj['youtubeId'] = $youtubeId;
-	}
-
-	public function get_video() {
-		return $this->video_obj;
-	}
-
-	public function set_comment($time, $text, $style) {
-		$comments = array("time"=>$time, "text"=>$text, "style"=>$style);
-		$this->video_obj['comments'][] = $comments;
-	}
-
-	public function delete_comment($time) {
-		//todo remove comment somehow from video_obj
-	}
-
-	public function get_comments() {
-	}
-
-}
-
-$new = new Video();
-$new->set_video(1, 'new video', 'new id');
-$new->set_comment(10, "new comment", "no style");
-$new->set_comment(20, "2nd comment", "second ytid");
-$new->delete_comment(10);
-echo json_encode($new->get_video(), JSON_PRETTY_PRINT);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
