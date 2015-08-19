df776bfd9792b80be80a7ea458f72757ef554b72
Jeff Lo <jeff@ve12sus.com>
2015-08-19 17:15:39 -0400

    start loadPlayer function

--- js/player.js
+++ js/player.js
@@ -1,6 +1,29 @@
-$(function() {
-	$("#button").click(function () {
-		$("#frame").attr("src", "http://www.youtube.com/embed/" +
-			 $("input#videoUrl").val() + "?rel=0&autoplay=1");
-	});
-});
+function loadPlayer() {
+	var tag = document.createElement('script');
+
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+	// 3. This function creates an <iframe> (and YouTube player)
+	//    after the API code downloads.
+	var player;
+	window.onYouTubeIframeAPIReady = function() {
+		player = new YT.Player('player', {
+			height: '390',
+			width: '640',
+			videoId: 'M7lc1UVf-VE',
+			events: {
+				'onReady': onPlayerReady,
+				//'onStateChange': onPlayerStateChange
+			}
+		});
+	}
+
+	// 4. The API will call this function when the video player is ready.
+	function onPlayerReady(event) {
+		event.target.playVideo();
+	}
+}
+
+loadPlayer();
